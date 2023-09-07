f364258b345a68175b5640d7c01a132c4c941666
Abid Hussain Anjum <Aanjum@atheeb-ingr.com>
2023-09-07 13:59:14 +0300

    change

---
+++ src/com/company/ResolveDiamonProblem/DemoClass.java
@@ -0,0 +1,30 @@
+package com.company.ResolveDiamonProblem;
+
+interface DemoInterface1
+{
+    public default void display()
+    {
+        System.out.println("the display() method of DemoInterface1 invoked");
+    }
+}
+interface DemoInterface2
+{
+    public default void display()
+    {
+        System.out.println("the display() method of DemoInterface2 invoked");
+    }
+}
+
+public class DemoClass implements DemoInterface1, DemoInterface2
+{
+    public static void main(String args[])
+    {
+        DemoClass obj = new DemoClass();
+        obj.display();
+    }
+
+    @Override
+    public void display() {
+        DemoInterface1.super.display();
+    }
+}
